a4fb4a7bbe6ad62fdaaf063c7f933bd75fec7800
Julie <lacognata@intechinfo.fr>
2016-05-10 08:27:30 +0200

    Add 2D array in Map

--- ITI.HistoryTreasures/ITI.HistoryTreasures/Map.cs
+++ ITI.HistoryTreasures/ITI.HistoryTreasures/Map.cs
@@ -8,16 +8,17 @@ namespace ITI.HistoryTreasures
 {
     class Map
     {
+        Level _levels;
+        Tile[,] _tileArray;
+
+        public Map()
+        {
+            Tile[,] _tileArray = new Tile[288, 288];
+        }
+
         public Level Level
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
+            get { return _levels; }
         }
     }
 }
