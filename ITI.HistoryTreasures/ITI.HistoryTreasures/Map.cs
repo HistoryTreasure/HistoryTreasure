7a19880799f3a0f0de5bc42ccb2d94a7acaf19b0
Julie <lacognata@intechinfo.fr>
2016-05-12 09:41:32 +0200

    Update Map

--- ITI.HistoryTreasures/ITI.HistoryTreasures/Map.cs
+++ ITI.HistoryTreasures/ITI.HistoryTreasures/Map.cs
@@ -6,18 +6,20 @@ using System.Threading.Tasks;
 
 namespace ITI.HistoryTreasures
 {
-    class Map
+    public class Map
     {
+        Level _level;
+        Tile[,] _tileArray;
+
+        public Map(Level level, int width, int height)
+        {
+            _level = level;
+            _tileArray = new Tile[width, height];
+        }
+
         public Level Level
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
+            get { return _level; }
         }
     }
 }
