c839c4b2e1afecfa22e6cf113ae11dd5803b0b41
YChergui <chergui@intechinfo.fr>
2016-05-24 09:49:19 +0200

    First add

--- ITI.HistoryTreasures.MapEditor/ITI.HistoryTreasures.MapEditor/Tile.cs
+++ ITI.HistoryTreasures.MapEditor/ITI.HistoryTreasures.MapEditor/Tile.cs
@@ -8,5 +8,21 @@ namespace ITI.HistoryTreasures.MapEditor.Core
 {
     public class Tile
     {
+        bool _isSolid;
+        TileEnum _tileName;
+
+        /// <summary>
+        /// Tile Constructor create Tile.
+        /// </summary>
+        /// <param name="isSolid">This parameter define if a tile can be cross.</param>
+        /// <param name="tileName">This parameter define the tileName.</param>
+        /// <param name="mapContext">This parameter define the MapContext.</param>
+        public Tile(bool isSolid, TileEnum tileName)
+        {
+            _isSolid = isSolid;
+            _tileName = tileName;
+        }
+
+
     }
 }
