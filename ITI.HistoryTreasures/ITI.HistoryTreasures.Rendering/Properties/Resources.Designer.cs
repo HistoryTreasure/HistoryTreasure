02286937516e942c357492c69e80d081226c4e1e
Julie <lacognata@intechinfo.fr>
2016-06-07 16:26:30 +0200

    Add book in resources

--- ITI.HistoryTreasures/ITI.HistoryTreasures.Rendering/Properties/Resources.Designer.cs
+++ ITI.HistoryTreasures/ITI.HistoryTreasures.Rendering/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace ITI.HistoryTreasures.Rendering.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap book {
+            get {
+                object obj = ResourceManager.GetObject("book", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap droite {
             get {
                 object obj = ResourceManager.GetObject("droite", resourceCulture);
