da17c14bd9a43e17bf4af3baf87c26fbb9102b03
madione <madione@intechinfo.fr>
2016-06-10 15:50:14 +0200

    creation of RiddleControl to show the riddle on the Form1.Designer
    modification of case map

---
+++ ITI.HistoryTreasures/ITI.HistoryTreasures.Rendering/RiddleControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITI.HistoryTreasures.Rendering
+{
+    public partial class RiddleControl : UserControl
+    {
+        Level _lCtx;
+        ResourcesManager _resourcesManager;
+
+        //RiddleManager _riddleManager;
+        public RiddleControl()
+        {
+            _resourcesManager = new ResourcesManager();
+            InitializeComponent();
+        }
+
+        public Level LevelContext
+        {
+            get { return _lCtx; }
+            set { _lCtx = value; }
+        }
+
+
+
+        private ResourcesManager GetResourcesManager
+        {
+            get { return _resourcesManager; }
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            e.Graphics.DrawString();
+        }
+
+
+
+
+    }
+}
