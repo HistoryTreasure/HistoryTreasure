f3afd89c96d47b8dbb5dd52ee3c7f549ff2a8c04
Julie <lacognata@intechinfo.fr>
2016-06-14 11:30:19 +0200

    Minor update

--- ITI.HistoryTreasures.MapEditor/ITI.HistoryTreasures.MapEditor/BitmapToXML.cs
+++ ITI.HistoryTreasures.MapEditor/ITI.HistoryTreasures.MapEditor/BitmapToXML.cs
@@ -11,42 +11,42 @@ namespace ITI.HistoryTreasures.MapEditor
     public class BitmapToXML
     {
         Bitmap b = Properties.Resources._base;
-
-
+        
         public void Translate()
         {
+            List<Color> _colors = new List<Color>();
             for (int i = 0; i < b.Height; i++)
             {
                 for (int j = 0; j < b.Width; j++)
                 {
-
                     Color p = b.GetPixel(i, j);
-                    if (p == Color.CornflowerBlue)
-                    {
-                        XElement map = new XElement("Map",
-                        new XElement("Tile", "Water")
-                          );
-                    }
-                    else if (p == Color.SandyBrown)
-                    {
-                        new XElement("Tile", "Floor");
-                    }
-                    else if (p == Color.Brown)
-                    {
-                        new XElement("Tile", "Bridge");
-                    }
-                    else if (p == Color.Black)
-                    {
-                        new XElement("Tile", "Home");
-                    }
-                    else if (p == Color.Violet)
-                    {
-                        new XElement("Tile", "PNJ");
-                    }
-                    else if (p == Color.Crimson)
-                    {
-                        new XElement("Tile", "Clue");
-                    }
+                    _colors.Add(p);
+                    //if (p == Color.CornflowerBlue)
+                    //{
+                    //    XElement map = new XElement("Map",
+                    //    new XElement("Tile", "Water")
+                    //      );
+                    //}
+                    //else if (p == Color.SandyBrown)
+                    //{
+                    //    new XElement("Tile", "Floor");
+                    //}
+                    //else if (p == Color.Brown)
+                    //{
+                    //    new XElement("Tile", "Bridge");
+                    //}
+                    //else if (p == Color.Black)
+                    //{
+                    //    new XElement("Tile", "Home");
+                    //}
+                    //else if (p == Color.Violet)
+                    //{
+                    //    new XElement("Tile", "PNJ");
+                    //}
+                    //else if (p == Color.Crimson)
+                    //{
+                    //    new XElement("Tile", "Clue");
+                    //}
                 }
             }
         }
