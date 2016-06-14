091ac526f69ac96e7ac23a9de8d4e8511a5edafe
Julie <lacognata@intechinfo.fr>
2016-06-14 10:02:11 +0200

    Update MapEditor -> try to create Map with bitmap file

---
+++ ITI.HistoryTreasures.MapEditor/ITI.HistoryTreasures.MapEditor/BitmapToXML.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ITI.HistoryTreasures.MapEditor
+{
+    public class BitmapToXML
+    {
+        Bitmap b = Properties.Resources._base;
+
+        public void Translate()
+        {
+            for (int i = 0; i < b.Height; i++)
+            {
+                for (int j = 0; j < b.Width; j++)
+                {
+                    b.GetPixel(i, j);
+                }
+            }
+        }
+    }
+}
