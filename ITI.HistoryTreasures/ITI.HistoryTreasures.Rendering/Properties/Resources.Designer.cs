c4880095c5c8f3f1da49decc7da28cb4667650c2
Julie <lacognata@intechinfo.fr>
2016-06-17 16:20:22 +0200

    Add resource rock

--- ITI.HistoryTreasures/ITI.HistoryTreasures.Rendering/Properties/Resources.Designer.cs
+++ ITI.HistoryTreasures/ITI.HistoryTreasures.Rendering/Properties/Resources.Designer.cs
@@ -188,5 +188,15 @@ namespace ITI.HistoryTreasures.Rendering.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rock {
+            get {
+                object obj = ResourceManager.GetObject("rock", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
