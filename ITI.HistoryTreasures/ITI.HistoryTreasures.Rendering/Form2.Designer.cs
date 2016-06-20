d1f07d13e899062fb3cf499fa149ee07a0695439
Julie <lacognata@intechinfo.fr>
2016-06-20 13:33:55 +0200

    Minor update

--- ITI.HistoryTreasures/ITI.HistoryTreasures.Rendering/Form2.Designer.cs
+++ ITI.HistoryTreasures/ITI.HistoryTreasures.Rendering/Form2.Designer.cs
@@ -37,7 +37,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(164, 275);
+            this.button1.Location = new System.Drawing.Point(433, 277);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -46,7 +46,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(213, 331);
+            this.button2.Location = new System.Drawing.Point(433, 324);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -56,7 +56,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(213, 406);
+            this.button3.Location = new System.Drawing.Point(433, 372);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
@@ -65,7 +65,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(163, 479);
+            this.button4.Location = new System.Drawing.Point(433, 415);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 3;
